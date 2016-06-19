023f0c8f2c5d5e419b12fe064a61d6748370cbc3
ShekharReddy4 <reddyshekhar619@gmail.com>
2016-06-20 01:28:29 +0530

    removed the markersites.js and cleaned some comments

--- app.js
+++ app.js
@@ -14,8 +14,9 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
+// favicon setup 
 app.use(favicon(path.join('public', 'images','favicon.ico')));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
