d32f39df682e550d7e07d15a8a1998ce36c4d833
ShekharReddy4 <reddyshekhar619@gmail.com>
2016-06-20 01:43:55 +0530

    unneccessary code

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
+// variables
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
-var markers = require('./models/markersites');
-
 var app = express();
 
 // view engine setup
@@ -16,14 +12,15 @@ app.set('view engine', 'ejs');
 
 // favicon setup 
 app.use(favicon(path.join('public', 'images','favicon.ico')));
-
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// routes
 app.use('/', routes);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
