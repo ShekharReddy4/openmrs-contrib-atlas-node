b6ab70e79429d627cdc0359605733db8afc3df97
ShekharReddy4 <reddyshekhar619@gmail.com>
2016-06-20 02:41:41 +0530

    added the markersites route

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var bodyParser = require('body-parser');
-var routes = require('./routes/index');
+
+var indexroute = require('./routes/index');
+var markersiteroute = require('./routes/markersites');
 var app = express();
 
 // view engine setup
@@ -12,14 +14,14 @@ app.set('view engine', 'ejs');
 
 // favicon setup 
 app.use(favicon(path.join('public', 'images','favicon.ico')));
-app.use(logger('dev'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // routes
-app.use('/', routes);
-
+app.use('/', indexroute);
+app.use('/markersites', markersiteroute);
 
 
 // catch 404 and forward to error handler
---
+++ routes/markersites.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+var mysql      = require('mysql');
+
+// set the mysql properties
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'root',
+    database : 'atlas'
+});
+
+//get the conncetion
+connection.connect();
+
+/* GET all the markersites */
+router.get('/', function(req, res, next) {
+
+    connection.query("select * from atlas", function (error, rows, field) {
+        if(!!error){
+            throw err;
+        }
+        else{
+            console.log(rows);
+        }
+    });
+
+    res.render('index', { title: 'OpenMRS Atlas' });
+});
+
+module.exports = router;
+
+
+
