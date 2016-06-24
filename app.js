a5e0a88d30a6a55be1ec13fd7aa079fc1a977c13
ShekharReddy4 <reddyshekhar619@gmail.com>
2016-06-24 17:27:38 +0530

    added route for creating a marker on atlas

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.set('view engine', 'ejs');
 app.use(favicon(path.join('public', 'favicon.ico')));
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(morgan('dev'));//this package logs the requests and errors in the console (needed for dev not intended for production purpose)
 
--- routes/markerSites.js
+++ routes/markerSites.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var mysql      = require('mysql');
+var uuid = require('uuid');
 
 // set the mysql properties
 var connection = mysql.createConnection({
@@ -45,6 +46,7 @@ router.get('/markerSites', function(req, res, next) {
 router.get('/marker/:id', function (req, res, next) {
     
     var id=req.params['id'];
+    console.log(id);
     connection.query('select * from atlas where id=?',[id], function (error, rows, field) {
 
         if(!!error){
@@ -78,22 +80,42 @@ router.get('/markers', function (req, res, next) {
 });
 
 //POST /marker/:id
-router.post('/markers:id', function (req, res, next) {
+router.post('/marker/', function (req, res, next) {
+    var id=uuid.v4();
+    var latitude=req.body.latitude;
+    var longitude=req.body.longitude;
+    var name=req.body.name;
+    var url=req.body.url;
+    var type=req.body.type;
+    var image=req.body.image;
+    var patients=req.body.patients;
+    var encounters=req.body.encounters;
+    var observations=req.body.observations;
+    var contact=req.body.contact;
+    var email=req.body.email;
+    var notes=req.body.notes;
+    var data=req.body.data;
+    var atlas_verison=req.body.atlas_version;
+    var date_created= new Date().toISOString().slice(0, 19).replace('T', ' ');
+    var date_changed=new Date().toISOString().slice(0, 19).replace('T', ' ');
+    var created_by=req.body.created_by;
+    var show_counts=req.body.show_counts;
+    var openmrs_version=req.body.openmrs_version;
+    var distribution=req.body.distribution;
 
-    var id=req.params['id'];
-    console.log(id);
-    connection.query('select * from atlas where type=? and openmrs_version=? and distribution=?',[types,versions,dists], function (error, rows, field) {
+    console.log(id+"    "+latitude+longitude+name+url+type+image+patients+encounters+date_changed+"           "+date_created);
 
+    connection.query('insert into atlas values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', [id,latitude,longitude,name,url,type,image,patients,encounters,observations,contact,email,notes,data,atlas_verison,date_created,date_changed,created_by,show_counts,openmrs_version,distribution], function (error, rows,field) {
         if(!!error){
             console.log(error);
         }
         else {
             res.setHeader('Content-Type', 'application/json');
-            res.json(rows);
-            //connection.end();
+            res.json(id);
         }
-    })
+    });
 });
+
 
 //close the connection
 //connection.end();
