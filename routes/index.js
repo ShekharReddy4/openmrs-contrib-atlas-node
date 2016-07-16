56b6c1a75356487bc86a40dc5f1a46b281d85218
ShekharReddy4 <reddyshekhar619@gmail.com>
2016-07-17 04:08:49 +0530

    added the lock for edit info window

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {
-    title: 'OpenMRS Atlas',
-    site: req.query.site,
-    positioin: req.query.position,
 
-  });
+    res.render('index', {
+      title: 'OpenMRS Atlas'
+    });
+  //  console.log(((req.query.zoom)*1) + req.query.clusters+req.query.legend )
+
 });
 
 module.exports = router;
