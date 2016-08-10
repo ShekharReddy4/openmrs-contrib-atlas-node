d0fa18d611bf42f05084beb1cffaf4f7e1d14b97
ShekharReddy4 <reddyshekhar619@gmail.com>
2016-08-10 13:06:54 +0530

    added the atlas-node-multipass

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,8 +1,9 @@
-var Multipass = require('multipass');
-
+var Multipass = require('atlas-node-multipass');
+var apikey = '1234567890abcdef';
+var sitekey = 'localhost';
+var multipass = new Multipass(apikey, sitekey);
 var express = require('express');
 var router = express.Router();
-
 
 //GET login
 router.get('/login', function(req, res, next) {
@@ -11,14 +12,9 @@ router.get('/login', function(req, res, next) {
 });
 
 module.exports = router;
-
-var apikey = '1234567890abcdef';
-var sitekey = 'localhost';
-//var apikey = 'API-KEY';
-//var sitekey = 'SITE-KEY';
-var multipass = new Multipass(apikey, sitekey);
-
 router.get('/auth/multipass/callback', function (req, res, next) {
     
+    var token = req.query.multipass;
+    var obj = multipass.decode(token);
     res.redirect('http://localhost:3001/');
 });
